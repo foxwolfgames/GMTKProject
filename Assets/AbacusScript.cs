c156e37fc340e4e2db5273f1cba6375cf417aba7
Linda Ly <lly022@ucr.edu>
2024-08-17 15:41:46 -0700

    Create object spawner w/ fixed spawn rate

--- Assets/AbacusScript.cs
+++ Assets/AbacusScript.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 public class AbacusScript : MonoBehaviour
 {
     public Rigidbody abacusRigidBody;
-    public GameObject abacus;
-    public float moveSpeed = 5;
 
     // Start is called before the first frame update
     void Start() 
     {
-        
-        transform.position = new Vector3(Random.Range(-22.0f, 22.0f), 30.0f, Random.Range(15.0f, -28.0f));
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
+
