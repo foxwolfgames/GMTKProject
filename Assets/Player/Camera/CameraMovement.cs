e9086e9d36833b99abbca3f02cc47cf08c16568a
Coco <cocochen12301@gmail.com>
2024-08-17 13:55:07 -0700

    feat: Input, movement, and camera

---
+++ Assets/Player/Camera/CameraMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform cameraPosition;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
