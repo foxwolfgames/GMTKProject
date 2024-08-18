9bf7287e01e4cc253bf8c02aeee76341ec408b7d
Coco <cocochen12301@gmail.com>
2024-08-17 22:22:23 -0700

    feat: Only last music note breaks glass

--- Assets/Scripts/Objects/MusicNote/MusicNotes.cs
+++ Assets/Scripts/Objects/MusicNote/MusicNotes.cs
@@ -6,21 +6,32 @@ public class MusicNotes : MonoBehaviour
 {
     [Header("Spawns")]
     public GameObject musicNote;
+    public GameObject musicNoteShatter;
     public int number = 7;
     private int currNumber;
-    public float spawnCooldown = .25f;
+    public float spawnCooldown1 = .25f;
+    public float spawnCooldown2 = .5f;
     void Start()
     {
         currNumber = number;
-        SpawnObject();
+        SpawnObject1();
     }
 
-    private void SpawnObject()
+    private void SpawnObject1()
     {
         Instantiate(musicNote, transform.position, transform.rotation, transform);
-        if(--currNumber > 0)
+        if (--currNumber > 1)
         {
-            Invoke(nameof(SpawnObject), spawnCooldown);
+            Invoke(nameof(SpawnObject1), spawnCooldown1);
         }
+        else if (--currNumber <= 0)
+        {
+            Invoke(nameof(SpawnObject2), spawnCooldown2);
+        }
+    }
+
+    private void SpawnObject2()
+    {
+        Instantiate(musicNoteShatter, transform.position, transform.rotation, transform);
     }
 }
