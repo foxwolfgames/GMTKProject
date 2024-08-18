756f4dc86685e9f03c770b31ca904cc8f7088f12
Linda Ly <lly022@ucr.edu>
2024-08-18 12:39:35 -0700

    Create object pool

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool SharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+    //shouldExpand = object pool for this object can be extended automatically
+    public bool shouldExpand = true;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject temp;
+        for(int i = 0; i < amountToPool; i++)
+        {
+            temp = Instantiate(objectToPool);
+            temp.SetActive(false);
+            pooledObjects.Add(temp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for(int i = 0; i < amountToPool; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+}
