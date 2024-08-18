8803be440be0a23b1b7516b489f798f18c25b662
Coco <cocochen12301@gmail.com>
2024-08-18 04:14:43 -0700

    refactor: Note shatter event

---
+++ Assets/Scripts/Objects/Glass/NoteShatterEvent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteShatterEvent : IEvent
+{
+    public Vector3 Position;
+    public float Radius;
+
+    public NoteShatterEvent(Vector3 position, float radius)
+    {
+        Position = position;
+        Radius = radius;
+    }
+
+    public void Invoke()
+    {
+        ScaleGame.Instance.EventRegister.InvokeNoteShatterEvent(this);
+    }
+
+}
