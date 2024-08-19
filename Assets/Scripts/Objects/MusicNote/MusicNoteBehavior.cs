8441c9c35197173cf0c800b135610660e4759020
Coco <cocochen12301@gmail.com>
2024-08-18 23:11:38 -0700

    feat: Music note prefab

--- Assets/Scripts/Objects/MusicNote/MusicNoteBehavior.cs
+++ Assets/Scripts/Objects/MusicNote/MusicNoteBehavior.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class MusicNoteBehavior : MonoBehaviour
 {
-    public float gravityModifier = 0.2f;
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("MusicNote"))
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
