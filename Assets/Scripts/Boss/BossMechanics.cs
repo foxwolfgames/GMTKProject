52ab2601c8a5f9c1f361a661ad5d07b97b61e89f
Coco <cocochen12301@gmail.com>
2024-08-20 08:20:18 -0700

    feat: Basic boss mechanics

---
+++ Assets/Scripts/Boss/BossMechanics.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMechanics : MonoBehaviour
+{
+    public int healthPoints = 10;
+    public int phase2Threshold = 6;
+    public int phase3Threshold = 3;
+    public Transform heartTransform;
+    public int scaleItemLayer = 10;
+    public BossItemSpawn bossItemSpawnScript;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage()
+    {
+        healthPoints--;
+        print(healthPoints);
+        if (healthPoints <= phase2Threshold)
+        {
+            if (bossItemSpawnScript)
+            {
+                bossItemSpawnScript.spawnInterval = 4f;
+            }
+
+        }
+        else if (healthPoints <= phase3Threshold)
+        {
+            if (bossItemSpawnScript)
+            {
+                bossItemSpawnScript.spawnInterval = 3f;
+            }
+        }
+    }
+}
