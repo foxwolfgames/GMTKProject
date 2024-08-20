bc66e82c65ec51ef7340fafe65aec9307e80bcbf
Coco <56405172+cocochen01@users.noreply.github.com>
2024-08-20 05:46:01 -0700

    feat: Downloaded some assets and set up some Scale Items (#38)
    * feat: New Boss model
    
    * feat: Music note
    
    * feat: Apple scale
    
    * feat: Various objects

--- Assets/Scripts/Objects/MusicNote/MusicNoteBehavior.cs
+++ Assets/Scripts/Objects/MusicNote/MusicNoteBehavior.cs
@@ -3,6 +3,24 @@ using UnityEngine;
 
 public class MusicNoteBehavior : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float time;
+    public float frequency = .5f;
+    public float torque = .2f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(rb)
+        {
+            time += Time.fixedDeltaTime;
+            rb.AddTorque(Vector3.down * Mathf.Sin(time * frequency) * torque);
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("MusicNote"))
